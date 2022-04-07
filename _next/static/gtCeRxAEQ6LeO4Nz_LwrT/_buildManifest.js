a3fe6f240434feeb248eb399f262ab9aedad4e63
rriosper <roberto.rios.perea@outlook.com>
2022-04-07 21:01:57 +0000

    Deploying to gh-pages from @ rriosper/palette-maker@7da953029622f0b186b91698a35cd06acb0f2333 🚀

--- _next/static/gtCeRxAEQ6LeO4Nz_LwrT/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/308-e5679fdf02dce6c7.js","static/chunks/pages/index-ee134b0c6c96334d.js"],"/_error":["static/chunks/pages/_error-d742f979193aeae4.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
